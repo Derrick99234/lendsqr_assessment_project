5ec7d0ef8b33a899180a22ab63d7c094bb80f6b2
derrick99234 <pshubomi@gmail.com>
2024-06-21 13:03:35 +0100

    Done with the header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import logo from "../assets/Group.svg";
+import search from "../assets/search-icon.svg";
+import profile from "../assets/image 4.png";
+import profile_dropdown from "../assets/profile_dropdown.svg";
+import notification from "../assets/notification-icon.svg";
 import Input from "./Input";
 
 function Header() {
@@ -9,15 +13,29 @@ function Header() {
       <img src={logo} alt="" className="logo" />
       <div className="search">
         <Input
-          placeholder="Search"
+          placeholder="Search for anything"
           type="text"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <button>0</button>
+        <button>
+          <img src={search} alt="search icon" />
+        </button>
       </div>
-      <div>
+      <div className="setting">
         <span>Docs</span>
+        <img
+          src={notification}
+          className="notification"
+          alt="notification icon"
+        />
+        <div>
+          <img src={profile} className="profile" alt="profile image" />
+          <span>
+            Derrick
+            <img src={profile_dropdown} alt="dropdown menu" />
+          </span>
+        </div>
       </div>
     </header>
   );
